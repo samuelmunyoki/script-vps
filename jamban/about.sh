4e5394c1be95840401b005548e775b698baa6ef4
Samuel Munyoki Muthembwa <samuelmuthembwa@yahoo.com>
2024-06-13 17:11:45 +0300

    Update about.sh

--- jamban/about.sh
+++ jamban/about.sh
@@ -4,7 +4,7 @@ echo "Checking VPS"
 
 clear
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[0;100;33m        • AutoScript by JULAK BANTUR •            \e[0m"
+echo -e " \e[0;100;33m        • AutoScript by SAMUEL MUNYOKI •            \e[0m"
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
